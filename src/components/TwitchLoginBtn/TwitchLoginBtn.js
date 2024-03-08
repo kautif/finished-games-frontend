cb872bbbd7bf030acb7eaaba3d93317784eab049
Autif Kamal <kautif@gmail.com>
2024-03-07 19:35:31 -0600

    Added Twitch Login button

--- src/components/TwitchLoginBtn/TwitchLoginBtn.js
+++ src/components/TwitchLoginBtn/TwitchLoginBtn.js
@@ -4,10 +4,9 @@ import axios from "axios";
 export default function TwitchLoginBtn () {
         const handleLogin = async () => {
             const redirectUri = encodeURIComponent("http://localhost:4000/auth/twitch/callback");
-            window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=${REACT_APP_TWITCH_ID}&redirect_uri=${redirectUri}&response_type=code&scope=user:read:email`;
-
+            window.location.href = `https://id.twitch.tv/oauth2/authorize?client_id=x3gwkermcoxekyog2z220rnbme1c9b&redirect_uri=${redirectUri}&response_type=code&scope=user:read:email`;
             try {
-                // await axios.get(`https://id.twitch.tv/oauth2/authorize?client_id=x3gwkermcoxekyog2z220rnbme1c9b&redirect_uri=https://localhost:3000/auth/twitch/callback&response_type=code&scope=user:read:email`)
+                
             } catch(error) {
                 console.error(error);
             }
