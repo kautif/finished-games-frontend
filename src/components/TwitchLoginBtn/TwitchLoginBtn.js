59cdeebf35b6049a9eb006d1cee77bd06e917090
Autif Kamal <kautif@gmail.com>
2024-05-06 17:26:58 -0500

    updated backend URL for twitch login button

--- src/components/TwitchLoginBtn/TwitchLoginBtn.js
+++ src/components/TwitchLoginBtn/TwitchLoginBtn.js
@@ -3,7 +3,8 @@ import React from "react";
 const TwitchLoginBtn = () => {
   const handleLogin = () => {
     const { REACT_APP_TWITCH_ID } = process.env;
-    const redirectUri = encodeURIComponent("http://localhost:4000/auth/twitch/callback");
+    const backendURL = process.env.REACT_APP_NODE_BACKEND || "http://localhost:4000";
+    const redirectUri = encodeURIComponent(`${backendURL}/auth/twitch/callback`);
     const authUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${REACT_APP_TWITCH_ID}&redirect_uri=${redirectUri}&response_type=code&scope=user:read:email`;
     window.location.href = authUrl;
   };
