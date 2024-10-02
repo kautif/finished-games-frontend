66eb277d4c0aaf2bc576cbf3c67fe71fd3148962
Autif Kamal <kautif@gmail.com>
2024-10-01 22:16:47 -0500

    reverted logout on null data in auth component

--- src/components/AuthenticatedComponent/index.jsx
+++ src/components/AuthenticatedComponent/index.jsx
@@ -64,14 +64,14 @@ function AuthenticatedComponent() {
 
   return (
     <div> 
-      {data ? (
+      {data &&
         <div>
           {/* <h1>{JSON.stringify(data.message, null, 2)}</h1> */}
           <h1>{data.message}</h1>
           <h2>This data is from protected route</h2>
           <AuthenticatedNav />
         </div>
-      ) : logout()}
+      }
     </div>
   );
 }
