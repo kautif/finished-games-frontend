890dee4e2cebcd7c7d667b98184704aa5fcfb339
Autif Kamal <kautif@gmail.com>
2024-11-26 07:18:15 -0600

    notify and prevent feedback submission if day hasnt passed

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -3,35 +3,91 @@ import axios from "axios";
 import Form from 'react-bootstrap/Form';
 import { ToastContainer, toast } from 'react-toastify';
 import "./Feedback.css"
+import { current } from "@reduxjs/toolkit";
 
 export default function Feedback () {
     const [username, setUsername] = useState("");
     const [formData, setFormData] = useState({});
     const [topic, setTopic] = useState("games");
     const [message, setMessage] = useState("");
+    const [hasSubmittedRecently, setHasSubmittedRecently] = useState(false);
     const [feedbackSent, setFeedbackSent] = useState(false);
     const backendURL = process.env.REACT_APP_BACKEND_API_URL || "http://localhost:4000";
+    let twitchId = window.localStorage.getItem("twitchId");
 
     function notify () {
-        toast("Feedback Submitted");
+        toast("Feedback Submitted", {
+            autoClose: 1000,
+            position: "top-center",
+            onClose: () => {
+                window.location.reload();
+            }
+        });
     }
+
+    function notifyFeedbackDenied () {
+        toast("We've already received your feedback for today. Thank you", {
+            autoClose: 1000,
+            position: "top-center"
+        });
+    }
+
+    let feedback;
+    let feedbackMonth;
+    let feedbackDay;
+    let feedbackYear;
+    let currentMonth = new Date().getMonth() + 1;
+    let currentDay = new Date().getDate();
+    let currentYear = new Date().getFullYear();
+    async function getFeedback () {
+        await axios(`${backendURL}/feedback`, {
+            method: "get",
+            params: {
+                twitchId: twitchId
+            }
+        }).then(results => {
+            feedback = results;
+            for (let i = 0; i < feedback.data.response.length; i++) {
+                let datePosted = new Date(feedback.data.response[i].date);
+                feedbackMonth = datePosted.getMonth() + 1;
+                feedbackDay = datePosted.getDate();
+                feedbackYear = datePosted.getFullYear();
+                if (feedbackMonth === currentMonth && 
+                    feedbackYear === currentYear &&
+                    Math.abs(currentDay - feedbackDay) > 0
+                ) {
+                    setHasSubmittedRecently(true);
+                    break;
+                }
+            }
+        })
+    }
+
     async function handleSubmit (e) {
-        await axios.post(`${backendURL}/send-email`, formData)
+        if (!hasSubmittedRecently) {
+            await axios.post(`${backendURL}/send-email`, formData)
             .then(response => {
             }).catch(err => {
                 console.error("Error: ", err.message);
             })
-        // try {
-        //   } catch (error) {
-        //     console.error('There was an error sending the email!', error);
-        //   }
+        }
     }
 
     useEffect(() => {
+        getFeedback();
+        if (hasSubmittedRecently) {
+            notifyFeedbackDenied();
+        }
+    }, [hasSubmittedRecently])
+
+    useEffect(() => {
+        const date = new Date();
         setFormData({
             username: username,
+            twitchId: twitchId,
             topic: topic,
-            message: message
+            message: message,
+            date: date
         })
         setFeedbackSent(false);
     }, [username, message, topic])
@@ -47,8 +103,11 @@ export default function Feedback () {
     return (
         <div>
             <Form className="feedback-form" 
-                onSubmit={() => {
-                    console.log("submit");
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (hasSubmittedRecently) {
+                        return false;
+                    }
                 }}>
                 <input 
                     id="feedback-username"
@@ -91,6 +150,7 @@ export default function Feedback () {
                     className="feedback-field"
                     type="submit"
                     value="Submit"
+                    disabled={hasSubmittedRecently}
                     onClick={(e) => {
                         e.preventDefault();
                         handleSubmit(e);
